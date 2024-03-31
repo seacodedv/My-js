78f6fa0827b301b36c36ccc6271203447b9402af
seacodedv <103368212+seacodedv@users.noreply.github.com>
2024-03-31 08:17:00 +0000

    neew

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const id = 190190
+let email = "xyz@google.com"
+var pass = "op1op2op"
+city = "Bwn"
+
+//id = 9010//
+email = "ss@gmail.com"
+
+console.table([id,email, pass, city])
